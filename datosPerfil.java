40dcfe4523cfb62a69821a01465667178a6611c8
lilianaRodriguezRodriguez <dgliliana.rodriguez@gmail.com>
2024-07-31 18:41:19 -0600

    ingreso de titulo del dia miercoles 28

--- datosPerfil.java
+++ datosPerfil.java
@@ -4,8 +4,8 @@ public class datosPerfil {
     public static void main(String[] args){
         Scanner ingresoDatos = new Scanner(System.in);
         String titulo;
-        System.out.println("Agrega un titulo");
-        titulo = ingresoDatos.next();
+       System.out.println("Agrega un titulo");
+       titulo = ingresoDatos.nextLine();
         String descripcion;
         System.out.println("Agrega como te sentiste el dia de hoy");
         descripcion = ingresoDatos.nextLine();
